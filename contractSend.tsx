f7ec040d25a196e04e30a13c799d44fe522c44c6
JPD18 <116178605+JPD18@users.noreply.github.com>
2022-11-27 10:26:53 +0000

    Add files via upload

---
+++ contractSend.tsx
@@ -0,0 +1,69 @@
+import React, { useState, Dispatch, SetStateAction } from "react";
+import { TezosToolkit, WalletContract } from "@taquito/taquito";
+const dep: string = "2";
+const contractAddress: string = "KT1Vhhu3nE2VYkk6UzekSNTNUi1os8rgDe5w";
+
+interface UpdateContractProps {
+  contract: WalletContract | any;
+  setUserBalance: Dispatch<SetStateAction<any>>;
+  Tezos: TezosToolkit;
+  userAddress: string;
+  setStorage: Dispatch<SetStateAction<number>>;
+}
+const Transfers = ({
+  contract = contractAddress,
+  setUserBalance,
+  Tezos,
+  userAddress,
+  setStorage,
+}: UpdateContractProps) => {
+  const [recipient, setRecipient] = useState<string>(contractAddress);
+  const [amount, setAmount] = useState<string>(dep);
+  const [loading, setLoading] = useState<boolean>(false);
+
+  const sendDeposit = async (): Promise<void> => {
+    try {
+      const contract = await Tezos.wallet.at(contractAddress);
+      const op = await contract.methods.default(false).send();
+      await op.confirmation();
+      const newStorage: any = await contract.storage();
+      if (newStorage) setStorage(newStorage.toNumber());
+      setUserBalance(await Tezos.tz.getBalance(userAddress));
+    } catch (error) {
+      console.log(error);
+    } finally {
+    }
+  };
+  return (
+    <div id="transfer-inputs">
+      <input
+        type="text"
+        placeholder="Recipient"
+        value={contractAddress}
+        onChange={(e) => setRecipient(e.target.value)}
+      />
+      <input
+        type="number"
+        placeholder="cost:2"
+        value={amount}
+        onChange={(e) => setAmount(dep)}
+      />
+      <button
+        className="button"
+        disabled={!recipient && !amount}
+        onClick={sendDeposit}
+      >
+        {loading ? (
+          <span>
+            <i className="fas fa-spinner fa-spin"></i>&nbsp; Please wait
+          </span>
+        ) : (
+          <span>
+            <i className="far fa-paper-plane"></i>&nbsp; Send
+          </span>
+        )}
+      </button>
+    </div>
+  );
+};
+export default Transfers;
